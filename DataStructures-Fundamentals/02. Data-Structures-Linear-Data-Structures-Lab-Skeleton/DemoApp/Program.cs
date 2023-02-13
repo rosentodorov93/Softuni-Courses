016e78ba45dfa4c62d7d9323d294c5432eba8c20
Rosen Todorov <rosen.vt55@gmail.com>
2023-02-13 18:33:27 +0200

    created demo app for testing

---
+++ DataStructures-Fundamentals/02. Data-Structures-Linear-Data-Structures-Lab-Skeleton/DemoApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Problem01.List;
+
+namespace DemoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Problem02.Stack.Stack<int>();
+
+            var numbers = new int[]{ 1,3,4,5,6};
+
+            foreach (var number in numbers)
+            {
+                stack.Push(number);
+            }
+
+        }
+    }
+}
